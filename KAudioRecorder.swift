d21731b0bade8eade73ae8ca779e1aded8cd1d6a
Kenan Atmaca <loveiscode@gmail.com>
2017-08-31 13:00:49 +0300

    Create KAudioRecorder.swift

---
+++ KAudioRecorder.swift
@@ -0,0 +1,231 @@
+//
+//  KAudioRecorder
+//
+//  Copyright © 2017 Kenan Atmaca. All rights reserved.
+//  kenanatmaca.com
+//
+//
+
+import UIKit
+import AVFoundation
+
+class KAudioRecorder: NSObject {
+
+    
+    static var shared = KAudioRecorder()
+    
+    private var audioSession:AVAudioSession = AVAudioSession.sharedInstance()
+    private var audioRecorder:AVAudioRecorder!
+    private var audioPlayer:AVAudioPlayer = AVAudioPlayer()
+    private let settings = [AVFormatIDKey: Int(kAudioFormatMPEG4AAC), AVSampleRateKey: 12000, AVNumberOfChannelsKey: 1, AVEncoderAudioQualityKey: AVAudioQuality.high.rawValue]
+    fileprivate var timer:Timer!
+    
+    var isPlaying:Bool = false
+    var isRecording:Bool = false
+    var url:URL?
+    var time:Int = 0
+    var recordName:String?
+    
+    override init() {
+        super.init()
+
+        isAuth()
+    }
+    
+    private func recordSetup() {
+       
+        let newVideoName = getDir().appendingPathComponent(recordName?.appending(".m4a") ?? "sound.m4a")
+        
+        do {
+            
+            try audioSession.setCategory(AVAudioSessionCategoryPlayAndRecord, with: .defaultToSpeaker)
+            
+                audioRecorder = try AVAudioRecorder(url: newVideoName, settings: self.settings)
+                audioRecorder.delegate = self as AVAudioRecorderDelegate
+                audioRecorder.isMeteringEnabled = true
+                audioRecorder.prepareToRecord()
+            
+        } catch {
+            print("Recording update error:",error.localizedDescription)
+        }
+    }
+    
+    func record() {
+        
+        recordSetup()
+        
+        if let recorder = self.audioRecorder {
+            if !isRecording {
+                
+                do {
+                    try audioSession.setActive(true)
+                    
+                    time = 0
+                    timer = Timer.scheduledTimer(timeInterval: 1, target: self, selector: #selector(updateTimer), userInfo: nil, repeats: true)
+                
+                    recorder.record()
+                    isRecording = true
+   
+                } catch {
+                    print("Record error:",error.localizedDescription)
+                }
+            }
+        }
+    }
+    
+    @objc private func updateTimer() {
+        
+        if isRecording && !isPlaying {
+            
+            time += 1
+            
+        } else {
+            timer.invalidate()
+        }
+    }
+    
+    func stop() {
+        
+       audioRecorder.stop()
+    
+        do {
+            try audioSession.setActive(false)
+        } catch {
+            print("stop()",error.localizedDescription)
+        }
+    }
+    
+    func play() {
+        
+        if !isRecording && !isPlaying {
+            if let recorder = self.audioRecorder  {
+                
+                if recorder.url.path == url?.path && url != nil {
+                    audioPlayer.play()
+                    return
+                }
+                
+                do {
+                    
+                    audioPlayer = try AVAudioPlayer(contentsOf: recorder.url)
+                    audioPlayer.delegate = self as AVAudioPlayerDelegate
+                    url = audioRecorder.url
+                    audioPlayer.play()
+                    
+                } catch {
+                    print("play(), ",error.localizedDescription)
+                }
+            }
+            
+        } else {
+            return
+        }
+    }
+    
+    func play(name:String) {
+        
+        let bundle = getDir().appendingPathComponent(name.appending(".m4a"))
+        
+        if FileManager.default.fileExists(atPath: bundle.path) && !isRecording && !isPlaying {
+            
+            do {
+                
+                audioPlayer = try AVAudioPlayer(contentsOf: bundle)
+                audioPlayer.delegate = self as AVAudioPlayerDelegate
+                audioPlayer.play()
+                
+            } catch {
+                print("play(with name:), ",error.localizedDescription)
+            }
+            
+        } else {
+            return
+        }
+    }
+    
+    func delete(name:String) {
+        
+        let bundle = getDir().appendingPathComponent(name.appending(".m4a"))
+        let manager = FileManager.default
+        
+        if manager.fileExists(atPath: bundle.path) {
+            
+            do {
+                try manager.removeItem(at: bundle)
+            } catch {
+                print("delete()",error.localizedDescription)
+            }
+            
+        } else {
+            print("File is not exist.")
+        }
+    }
+    
+    func stopPlaying() {
+        
+        audioPlayer.stop()
+        isPlaying = false
+    }
+    
+    private func getDir() -> URL {
+        
+        let paths = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask)
+        
+        return paths.first!
+    }
+    
+    @discardableResult
+    func isAuth() -> Bool {
+        
+        var result:Bool = false
+        
+        AVAudioSession.sharedInstance().requestRecordPermission { (res) in
+            result = res == true ? true : false
+        }
+        
+        return result
+    }
+    
+    func getTime() -> String {
+        
+        var result:String = ""
+        
+        if time < 60 {
+            
+            result = "\(time)"
+            
+        } else if time >= 60 {
+            
+            result = "\(time / 60):\(time % 60)"
+        }
+        
+        return result
+    }
+    
+}//
+
+extension KAudioRecorder: AVAudioRecorderDelegate {
+    
+    func audioRecorderDidFinishRecording(_ recorder: AVAudioRecorder, successfully flag: Bool) {
+        isRecording = false
+        url = nil
+        timer.invalidate()
+        print("record finish")
+    }
+    
+    func audioRecorderEncodeErrorDidOccur(_ recorder: AVAudioRecorder, error: Error?) {
+        print(error.debugDescription)
+    }
+}
+
+extension KAudioRecorder: AVAudioPlayerDelegate {
+    
+    func audioPlayerDidFinishPlaying(_ player: AVAudioPlayer, successfully flag: Bool) {
+        isPlaying = false
+        print("playing finish")
+    }
+    
+    func audioPlayerDecodeErrorDidOccur(_ player: AVAudioPlayer, error: Error?) {
+        print(error.debugDescription)
+    }
+}
